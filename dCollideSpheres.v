b4b5a56a10b2ad66f7185f33246dec289bc62b5d
oakleyKatt <okatterheinrich@gmail.com>
2016-05-19 14:16:52 -0400

    Create dCollideSpheres.v

---
+++ dCollideSpheres.v
@@ -0,0 +1,12 @@
+module dCollideSpheres(x1, y1, z1, r1, x2, y2, z2, r2, cx, cy, cz, normalX, normalY, normalZ, depth, g1, g2, return);
+/**
+	dVector3 p1:		x1, y1, z1
+	dReal r1:			r1
+	dVector3 p2:		x2, y2, z2
+	dReal r2:			r2
+	dContactGeom *c:	
+		dVector3 pos		cx, cy, cz
+		dVector3 normal	normalX, normalY, normalZ
+		dReal depth			depth
+		dGeomID g1, g2		g1, g2
+*/
